640efd544897355802111d9416fb2e39670a3d5a
Sagar <m.shafiulalamsagar@gmail.com>
2026-08-26 04:36:52 +0600

    Update API base URL for production backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://mystore-sx6n.onrender.com',
 });
 
 // Automatically add JWT Bearer token to requests
